6a26cf43450ab9eb7701ee13f3f05cc3265d487b
Connor Lovitt <conkanator@hotmail.com>
2025-03-11 17:29:55 -0700

    3/11 Activation and Gradient Data

--- TrainingEnvironments/FeedForwardTrainingEnvironment.py
+++ TrainingEnvironments/FeedForwardTrainingEnvironment.py
@@ -18,6 +18,8 @@ class FeedForwardTrainingEnvironment():
         accuracy_record = []
         mean_activation_record = [ [] for layer in range(len(self.Model.hidden_shapes)) ]
         std_layer_activation_record = [ [] for layer in range(len(self.Model.hidden_shapes)) ]
+        mean_gradient_record = [ [] for layer in range(len(self.Model.hidden_shapes))]
+        std_gradient_record = [ [] for layer in range(len(self.Model.hidden_shapes))]
 
         for epoch in range(self.Epoch):
     
@@ -25,6 +27,7 @@ class FeedForwardTrainingEnvironment():
             correct = 0
             
             activation_data = [ [] for layer in range(len(self.Model.hidden_shapes)) ]
+            gradient_data = [ [] for layer in range(len(self.Model.hidden_shapes))]
 
             for batch in range(self.Batches):
 
@@ -45,17 +48,27 @@ class FeedForwardTrainingEnvironment():
                 loss = self.Criterion(output, labels)
                 cum_loss += loss.item()
                 loss.backward()
+
                 self.Optimizer.step()
-                
+
+                for l in range(len(self.Model.network)-1):
+                    
+                    current_layer = self.Model.network[l]
+                    if isinstance(current_layer, torch.nn.Linear):
+                        gradient_data[(l+1)//2].append(current_layer.weight.grad.norm())
+            
+            # Saving Data
             loss_record.append(cum_loss/self.Batches)
             accuracy_record.append(correct/self.Batches)
+
             for layer in range(len(activation_data)):
 
                 activation_data[layer] = torch.cat(activation_data[layer], 0)
+                gradient_data[layer] = torch.tensor(gradient_data[layer])
+
                 mean_activation_record[layer].append(float(torch.mean(activation_data[layer]).data))
                 std_layer_activation_record[layer].append(float(torch.std(activation_data[layer]).data))
-
-            #if epoch % 10 == 0:
-                #print(f'Epoch {epoch}, Loss: {loss.item():.4f}, Accuracy: {correct/self.Batches:.4f}')
+                mean_gradient_record[layer].append(float(torch.mean(gradient_data[layer]).data))
+                std_gradient_record[layer].append(float(torch.std(gradient_data[layer]).data))
         
-        return loss_record, accuracy_record, mean_activation_record, std_layer_activation_record
+        return loss_record, accuracy_record, mean_activation_record, std_layer_activation_record, mean_gradient_record, std_gradient_record
