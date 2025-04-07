29f2d9c82bccda52614dbe567f756f4ef4e145ca
Connor Lovitt <conkanator@hotmail.com>
2025-04-07 13:46:11 -0700

    4/7 Finished New Dataset Changes

--- Datasets/GaussianSample.py
+++ Datasets/GaussianSample.py
@@ -1,29 +1,31 @@
 #import torch
-from torch import rand, normal, ones, empty, abs
+import torch
 from torch.utils.data import Dataset
 import matplotlib.pyplot as plt
 
 class GaussianData(Dataset):
-    def __init__(self, midpoints):
+    def __init__(self, midpoints, std):
         super().__init__()        
         self.midpoints= midpoints
-
+        self.num_clusters = self.midpoints.size()[0]
+        self.num_dimensions = self.midpoints.size()[1]
+        self.std = std * torch.ones(self.num_dimensions)
     def __len__(self):
-        return self.midpoints.size()[0]
+        return self.num_clusters
 
     def __getitem__(self, index):
         mean = self.midpoints[index]
-        sample = abs(normal(mean=mean, std= (1/16) * ones(len(self))))
-        
+        sample = torch.normal(mean=mean, std= self.std )
+        sample = torch.clip(sample, 0, 1)
         return sample, index
 
     def showData(self):
         
         if self.num_dimensions == 2:
-            for dist in range(self.num_distributions):
-                index = dist * ones(100, dtype=int)
+            for dist in range(self.num_clusters):
+                index = dist * torch.ones(100, dtype=int)
                 input, _ = self.__getitem__(index)
-                plt.scatter(input[:, 0], input[:, 1], marker='o', label="Data Points"+str(dist))
+                plt.scatter(input[:, 0], input[:, 1], marker='o', label="Data Points"+str(dist+1))
             
             # Labels and title
             plt.xlabel("X-axis")
